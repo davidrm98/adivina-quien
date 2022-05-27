6f9581f27205e070d4aff56f999fc23e7bf94fe8
GGMxXero <18040424@itdurango.edu.mx>
2022-05-27 18:55:04 -0500

    primero

---
+++ main.py
@@ -0,0 +1,7 @@
+genero = input("¿De que genero es tu personaje?")
+if genero=="hombre":
+    raza = input("¿De que raza es tu personaje?")
+    if raza =="bioandroide":
+        print ("Tu personaje es Cell")
+    if raza =="demonio":
+        print ("Tu personaje es Super Boo")
