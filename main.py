487a999b76e2b5d445bcb9cbd153eea0da0afce5
GGMxXero <18040424@itdurango.edu.mx>
2022-05-27 19:41:52 -0500

    primeros personajes

--- main.py
+++ main.py
@@ -5,3 +5,31 @@ if genero=="hombre":
         print ("Tu personaje es Cell")
     if raza =="demonio":
         print ("Tu personaje es Super Boo")
+    if raza=="Shinjin":
+        print ("Tu personaje es Kaio-sama")
+
+    if raza =="humano":
+        edad = input("¿De que edad es tu personaje?")
+        if edad=="adulto":
+            color_cabello = input("¿De que color tiene el cabello tu personaje?")
+            if color_cabello=="n/a":
+                color_ropa = input("¿De que color tiene la ropa tu personaje?")
+                if color_ropa=="naranja":
+                    bello_facial = input("¿tu personaje tiene barba o bigote?")
+                    if bello_facial=="n/a":
+                        print ("Tu personaje es Krilin")
+                    if bello_facial=="barba":
+                        print ("Tu personaje es el Maestro Roshi")
+            if color_cabello=="negro":
+                color_ropa = input("¿De que color tiene la ropa tu personaje?")
+                if color_ropa=="naranja":
+                    print ("Tu personaje es Yamcha")
+                if color_ropa=="cafe":
+                    print ("Tu personaje es Mr.Satan")
+    if raza=="namekusei":
+        edad = input("¿De que edad es tu personaje?")
+
+    if raza=="sayajin":
+        edad = input("¿De que edad es tu personaje?")
+
+    
